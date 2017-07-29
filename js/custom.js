abb2597aa49b6b6a1cab52d21f3c507baa762839
BornGellote <icmanitoo@gmail.com>
2017-07-29 14:15:28 +0300

    add reg/restore form

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,3 @@
 
-$('input, textarea').placeholder();
-$("#tabs").on("click", "a", function(){
-    var me = $(this);
 
-    $(".active").removeClass("active");
-    me.addClass("active");
-    $("#" + me.attr("id").replace("tab", "container")).addClass("active");
-    return false;
-});
+
---
+++ js/index.js
@@ -0,0 +1,9 @@
+$("#tabs").on("click", "a", function(){
+  var me = $(this);
+
+  $(".active").removeClass("active");
+  me.addClass("active");
+  $("#" + me.attr("id").replace("tab", "container")).addClass("active");
+  return false;
+});
+$('input, textarea').placeholder();
